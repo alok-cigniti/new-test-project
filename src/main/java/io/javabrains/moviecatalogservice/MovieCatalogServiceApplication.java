89541ea0dac20a7872b872247de94e23c0383974
Alok Bhawsinka <alok.bhawsinka@cigniti.com>
2021-07-15 13:57:17 +0530

    envirnment variable commit

--- src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,10 +2,6 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 
@@ -14,7 +10,5 @@ public class MovieCatalogServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
-
-
 }
 
--- src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,6 +1,8 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +12,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
+@Profile("dev")
 public class CatalogResource {
+	@Value("${value.from.file}")
+	private String valueFromFile;
 
-    @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Arrays.asList(new CatalogItem("Alok", "developer", 100),
-                new CatalogItem("Manoj", "testing", 101),
-                new CatalogItem("Alok", "testing", 105),
-                new CatalogItem("Shivam", "architect", 102));
-    }
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+		return Arrays.asList(new CatalogItem("Alok", "developer", 100),
+				new CatalogItem("Manoj", "testing", 101),
+				new CatalogItem("Alok", "testing", 105),
+				new CatalogItem("Shivam", "architect", 102));
+	}
 
-    @RequestMapping("/count")
-    public int getCatalogSize() {
-        return Arrays.asList(new CatalogItem("Alok", "developer", 100),
-                new CatalogItem("Manoj", "testing", 101),
-                new CatalogItem("Shivam", "architect", 102)).size();
-    }
+	@RequestMapping("/count")
+	public int getCatalogSize() {
+		return Arrays.asList(new CatalogItem("Alok", "developer", 100),
+				new CatalogItem("Manoj", "testing", 101),
+				new CatalogItem("Shivam", "architect", 102)).size();
+	}
+
+	@RequestMapping("/env")
+	public String getEnvValue() {
+		return valueFromFile;
+	}
 }
